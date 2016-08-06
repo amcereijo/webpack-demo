fe1d5afaf637c0a2dea071c890f55e6c4f6fca18
amcereijo <amcereijo@gmail.com>
2016-08-06 18:39:13 +0200

    Finished chapter "Developing with Webpack"

---
+++ app/index.js
@@ -0,0 +1,9 @@
+var component = require('./component');
+
+let styles = require('./main.css'); 
+
+// const componentE = component();
+// document.body.appendChild(componentE);
+// componentE.className = styles.redButton;
+
+document.body.appendChild(component());
